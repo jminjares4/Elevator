604f62e5d9e93a802b5ae281bd43ab645ad3bf3f
jminjares4 <jminjares4@miners.utep.edu>
2021-11-27 18:26:14 -0700

    Added .gitignore and edit main.c

--- Elevator/main/main.c
+++ Elevator/main/main.c
@@ -16,29 +16,7 @@
 #include "inc/motion_sensor.h"
 #include "inc/button.h"
 
-extern Accelerometer acc;
-extern PressureSensor pressSens;
-extern Led onboard;
-extern Servo SG90;
-extern MotionSensor hc_sr501;
-
-
-void task5(void *arg)
-{
-    // const uart_port_t uart_num = UART_NUM_2;
-    while (1)
-    {
-        // Write data to UART.
-        char *test_str = "Jesus Minjares External UART API!\r\n";
-        // uart_write_bytes(uart_num, (const char *)test_str, strlen(test_str));
-        serial_write(&externalSerial,test_str,strlen(test_str));
-        vTaskDelay(1000 / portTICK_RATE_MS);
-    }
-}
-
 void app_main(void)
 {
-    serialCommInit(&externalSerial);
-
-    xTaskCreate(&task5, "task 5", 2048, NULL, 5, NULL);
+  
 }
